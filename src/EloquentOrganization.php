d3490c11132f131fbe2755a654b2852b2bb8218f
ejaxon <eric@deepweave.com>
2015-03-17 16:13:35 -0400

    Fixed tmp bug

--- src/EloquentOrganization.php
+++ src/EloquentOrganization.php
@@ -5,7 +5,7 @@ use Illuminate\Contracts\Auth\Authenticatable as UserContract;
 /*
  * EloquentOrganization implements the Organization interface when the org is an Eloquent model
  */
-class EloquentOrganization implements Organization
+trait EloquentOrganization implements Organization
 {
 
     private function getOrgMemberClass()
--- src/MultiOrgManager.php
+++
@@ -1,5 +0,0 @@
-<?php namespace DemocracyApps\MultiOrg;
-
-class MultiOrgManager {
-
-}
